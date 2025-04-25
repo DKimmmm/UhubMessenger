716b5851ab9480e102149a721dee05841e7a0163
danil-kkrk <hirofool502@gmail.com>
2025-04-25 23:56:37 +0300

    validation service refactoring

--- src/main/java/com/example/uhabmessenger/validation/EmailFormatValidator.java
+++ src/main/java/com/example/uhabmessenger/validation/EmailFormatValidator.java
@@ -3,14 +3,10 @@ package com.example.uhabmessenger.validation;
 import com.example.uhabmessenger.validation.annotation.EmailFormat;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static com.example.uhabmessenger.formatutils.RegexUtils.EMAIL_REGEX;
 
 public class EmailFormatValidator implements ConstraintValidator<EmailFormat, String> {
-
-    private static final Logger log = LoggerFactory.getLogger(EmailFormatValidator.class);
 
     @Override
     public void initialize(EmailFormat constraintAnnotation) {
@@ -22,12 +18,13 @@ public class EmailFormatValidator implements ConstraintValidator<EmailFormat, St
         if (value == null || value.trim().isEmpty()) {
             return true;
         }
-
         return matchersEmailRegex(value);
+
     }
 
     public static boolean matchersEmailRegex(String value) {
-        return value.matches(EMAIL_REGEX);
-    }
 
+        return value.matches(EMAIL_REGEX);
+
+    }
 }
