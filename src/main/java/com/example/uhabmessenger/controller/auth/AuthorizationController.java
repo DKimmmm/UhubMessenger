e980de0aa588b7567581ef82c51af069b3a4c6dd
KoDi586 <138789200+KoDi586@users.noreply.github.com>
2025-05-12 14:45:53 +0300

    cloud feign add test

--- src/main/java/com/example/uhabmessenger/controller/auth/AuthorizationController.java
+++ src/main/java/com/example/uhabmessenger/controller/auth/AuthorizationController.java
@@ -8,10 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -38,4 +35,10 @@ public class AuthorizationController {
                     authUserService.login(loginDto)
             );
     }
+
+    @GetMapping("/test/string")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("my post line");
+    }
+
 }
